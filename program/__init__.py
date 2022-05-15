7db26c5df704a7fcee39a778052562a0e143d4af
BartekAndree <73586879+BartekAndree@users.noreply.github.com>
2022-05-15 16:41:43 +0200

    Add files via upload

--- program/__init__.py
+++ program/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from os import environ
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = '6d84712a50d8a51fbde362773dd218eb'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
